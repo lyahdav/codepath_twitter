98fbb4ce823f4c9bde7dcece8c7dd8069317e07b
Liron Yahdav <lyahdav@gmail.com>
2014-03-30 17:29:11 -0700

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Liron Yahdav on 3/30/14.
+//  Copyright (c) 2014 Liron Yahdav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
